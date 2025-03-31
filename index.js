e68ac27c46fa74270d1efcdc8f6686404428e307
stephanonisick01 <89089211+stephanonisick01@users.noreply.github.com>
2025-03-31 09:53:55 -0500

    New Stuff

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const htmlContent = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello GitHub Codespaces</title>
+    <title>Hello GitHub Codespaces!!</title>
     <style>
         body {
             font-family: Arial, sans-serif;
